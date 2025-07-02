b90394c21779cf0da7f5219b61405180967e7478
Juan Ibiapina <juanibiapina@gmail.com>
2025-07-02 10:20:36 +0200

    feat: Configure Prisma Postgres

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,26 @@
 import { Hono } from "hono";
+import { PrismaClient } from "../generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+export interface Env {
+  DATABASE_URL: string;
+}
+
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+app.get("/api/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const user = await prisma.user.create({
+    data: {
+      email: `Jon${Math.ceil(Math.random() * 1000)}@gmail.com`,
+      name: "Jon Doe",
+    },
+  });
+
+  return c.json({name: user.name})
+});
 
 export default app;
