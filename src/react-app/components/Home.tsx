7bf01d3682d7f82da6bc1edf544b52c3638d41b3
Juan Ibiapina <juanibiapina@gmail.com>
2025-07-05 22:17:39 +0200

    Remove styling completely and install tailwind

--- src/react-app/components/Home.tsx
+++ src/react-app/components/Home.tsx
@@ -1,24 +1,21 @@
 import { Session } from '@auth/core/types';
 import { signOut } from '@hono/auth-js/react';
 
-import './Home.css';
-
 const Home = ({ session }: { session: Session }) => {
   return (
-    <div className="home-container">
-      <h1 className="home-title">Hey Lovely Girl ❤️</h1>
+    <div>
+      <h1>Hey Lovely Girl ❤️</h1>
 
       {session.user?.image && (
-        <div className="profile-image-block">
+        <div>
           <img
             src={session.user.image}
             alt="Profile"
-            className="profile-image"
           />
         </div>
       )}
 
-      <button className="signout-button" onClick={() => signOut()}>
+      <button onClick={() => signOut()}>
         Sign Out
       </button>
     </div>
