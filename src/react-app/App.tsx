02a536bd61a775fdf45cdaa645af7c988648515d
Juan Ibiapina <juanibiapina@gmail.com>
2025-07-07 22:05:53 +0200

    refactor: Switch react-router to data mode

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,18 +1,10 @@
 import { SessionProvider } from '@hono/auth-js/react';
-import { BrowserRouter, Routes, Route } from "react-router";
-
-import Home from "./Home.tsx";
-import TripPage from "./components/TripPage.tsx";
+import { Outlet } from "react-router";
 
 function App() {
   return (
     <SessionProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/trips/:tripId" element={<TripPage />} />
-        </Routes>
-      </BrowserRouter>
+      <Outlet />
     </SessionProvider>
   );
 }
--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,11 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 
 import "./main.css";
 import App from "./App.tsx";
+import Home from "./Home.tsx";
+import TripPage from "./components/TripPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "trips/:tripId", element: <TripPage /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
+
