134ddd60fbad5efb793f974827ca0b1df41a9f02
Juan Ibiapina <juanibiapina@gmail.com>
2025-07-31 09:42:38 +0200

    Revert "test: Configure testing for cloudflare workers"
    This reverts commit 3d742b61077916a4c8fa7e3bd70ae620c9df1dc7.
    
    Because vitest pool workers isn't compatible with nodejs.
    
    https://github.com/cloudflare/workers-sdk/issues/7324

--- test/env.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Env } from "../src/worker/index";
-
-declare module "cloudflare:test" {
-  // ProvidedEnv controls the type of `import("cloudflare:test").env`
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface ProvidedEnv extends Env {}
-}
